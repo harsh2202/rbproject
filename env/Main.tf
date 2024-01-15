a3a6dcb39c79a36b45c64ff95a0edef894bbe71e
Harish Kumar <harish.h.kumar@mercedes-benz.com>
2024-01-15 17:48:03 +0100

    update

--- env/Main.tf
+++ env/Main.tf
@@ -9,10 +9,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  # subscription_id   = "4fdbc0b9-1163-442d-96e8-0edc10214845"
-  # tenant_id         = "be834229-c698-4960-a9c3-0fe2746998b0"
-  # client_id         = "496514d5-ef37-4893-9fd3-fa4a1e52c7f3"
-  # client_secret     = "OjX8Q~qjj7wwHdlzg1fsg~sIqyTKHosaX6qZRdyh"
+  # subscription_id   = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  # tenant_id         = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  # client_id         = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  # client_secret     = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
 module "network" {
